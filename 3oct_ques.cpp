ed42d9dcfe35e13367e3593b115bbd28828b348c
Trapti Patel <114275270+trapti77@users.noreply.github.com>
2023-10-03 20:30:57 +0530

    number of good pairs

---
+++ 3oct_ques.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int c=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+};
