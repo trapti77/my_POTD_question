8cec10040aa2862bb2e9e2b09417f1111e8b8f33
Trapti Patel <114275270+trapti77@users.noreply.github.com>
2023-12-01 21:47:29 +0530

    Add files via upload

---
+++ leet3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool solve(vector<string> &v1, vector<string> &v2)
+{
+  vector<char> ch1;
+  vector<char> ch2;
+  for (int i = 0; i < v1.size(); i++)
+  {
+    for (int j = 0; j < v1[i].size(); j++)
+    {
+      ch1.push_back(v1[i][j]);
+    }
+  }
+  for (int i = 0; i < v2.size(); i++)
+  {
+    for (int j = 0; j < v2[i].size(); j++)
+    {
+      ch2.push_back(v2[i][j]);
+    }
+  }
+
+  for (int i = 0, j = 0; i < ch1.size(), j < ch2.size(); i++, j++)
+  {
+    if (ch1[i] != ch2[j])
+    {
+      return false;
+    }
+  }
+  return true;
+}
+int main()
+{
+  int n;
+  cin >> n;
+  vector<string> v1(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> v1[i];
+  }
+  vector<string> v2(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> v2[i];
+  }
+  cout << solve(v1, v2);
+  return 0;
+}
